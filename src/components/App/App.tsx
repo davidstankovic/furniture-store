6dc3181ade92cc7b566bb9515a25be22ffb80ae5
David <david.stankovic.16@singimail>
2020-05-27 21:28:11 +0100

    Created the main menu component -static

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
-import { faHome, faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
     <Container>
+      
        <FontAwesomeIcon icon={ faHome } /> Home
     </Container>
   );
